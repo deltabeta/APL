29391a32548d24e84e8ccfc6223f74e13978c4d1
walid radhouane <radhouane.walid.m2@gmail.com>
2014-07-16 09:42:01 +0100

    MAJ 16 07 09/41

---
+++ protected/views/layouts/layoutadmin.php
@@ -0,0 +1,178 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="en" />
+
+        <!-- blueprint CSS framework -->
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+        <!--[if lt IE 8]>
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+        <![endif]-->
+
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+
+
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
+    <!-- hi alter !! -->
+    <body>
+        <!-- Hello Ayman -->
+        <!-- ich bedanke mich fuer dzd buch -->
+        <!-- Hello Seif 1 -->
+
+
+
+        <!--  alooooo -->
+
+        <!-- Hello Ayman deutsh-->
+
+        <!-- Hello Seif -->
+
+
+        <div class="container" id="page">
+
+            <div id="header">
+                <div id="logo">
+
+                    <?php echo CHtml::encode(Yii::app()->name); ?></div>
+            </div><!-- header -->
+
+            
+            
+            
+            <div id="mainmenu">
+                <?php
+                $this->widget(
+                        'booster.widgets.TbNavbar', array(
+                    'type' => '',
+                    'brand' => '<img src="' . Yii::app()->request->baseUrl . '/images/logo-africanpress.png" width="150" />',
+                    'brandUrl' => array('/site/index'),
+                    'collapse' => true, // requires bootstrap-responsive.css
+                    // 'fixed' => false,
+                    'fluid' => true,
+                    'items' => array(
+                        array(
+                            'class' => 'booster.widgets.TbMenu',
+                            'type' => 'navbar',
+                            'items' => array(
+                                array('label' => 'Home ', 'url' => array('/site/index')),
+                                array(
+                                    'label' => 'For who and how?',
+                                    'url' => Yii::app()->request->baseUrl . '/site/howtouse',
+                                    'items' => array(
+                                        array('label' => 'For who and how', 'url' => array('/site/forwhoandhow')),
+                                        array('label' => 'How to use', 'url' => array('/site/howtouse')),
+                                        array('label' => 'Tips to write', 'url' => array('/site/tipstowrite')),
+                                        array('label' => 'Press', 'url' => '#'),
+                                    )
+                                ),
+                                array('label' => 'Costs of Use', 'url' => array('/site/costofuse')),
+                                array(
+                        'label' => 'About Us',
+                        'url' => '#',
+                        'items' => array(
+                            array('label' => 'About the African Press List', 'url' => array('/site/about')),
+                            array('label' => 'Contact us', 'url' =>  array('/site/contact')),
+                        )
+                                    )
+                            ),
+                        ),
+                        //'<form class="navbar-form navbar-left" action=""><div class="form-group"><input type="text" class="form-control" placeholder="Search"></div></form>',
+                        array(
+                            'class' => 'booster.widgets.TbMenu',
+                            'type' => 'navbar',
+                            'htmlOptions' => array('class' => 'pull-right'),
+                            'items' => array(
+                                '--',
+                            array('label' => ' Sign-Up', 'items' => array(
+                                        array('label' => 'As Client', 'url' => array('/user/create')),
+                                        array('label' => 'As Journalist ', 'url' => array('/contact/create')),
+                                    )
+                                    , 'visible' => Yii::app()->user->isGuest),
+                            array('label' => 'My Space', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                            array('label' => 'Settings ', 'visible' => !Yii::app()->user->isGuest, 'items' => array(
+                                array('label' => 'My Profile',), 
+                                   //'url' => array('update', 'id' => $model->contact_id) //'visible' => !Yii::app()->user->isGuest), 
+                                   //or 'url' => array('/contact/view/id'),
+                                array('label' => 'Logout', 'url' => array('/site/logout'),)// 'visible' => !Yii::app()->user->isGuest)    
+                            ),),),
+                        ),
+                    ),
+                        )
+                );
+                ?>
+
+
+
+            </div>
+            <div id="mainmenu">
+                <?php
+                // Yii User 
+//                $this->widget('zii.widgets.CMenu', array(
+//                    'items' => array(
+//                        array('url' => Yii::app()->getModule('user')->loginUrl, 'label' => Yii::app()->getModule('user')->t("Login"), 'visible' => Yii::app()->user->isGuest),
+//                        array('url' => Yii::app()->getModule('user')->registrationUrl, 'label' => Yii::app()->getModule('user')->t("Register"), 'visible' => Yii::app()->user->isGuest),
+//                        array('url' => Yii::app()->getModule('user')->profileUrl, 'label' => Yii::app()->getModule('user')->t("Profile"), 'visible' => !Yii::app()->user->isGuest),
+//                        array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
+//                    ),
+//                ));
+                ?>
+            </div>
+                <?php if (isset($this->breadcrumbs)): ?>
+                    <?php
+                    $this->widget('zii.widgets.CBreadcrumbs', array(
+                        'links' => $this->breadcrumbs,
+                    ));
+                    ?><!-- breadcrumbs -->
+                <?php endif ?>
+
+
+
+                <?php echo $content; ?>
+
+
+
+
+            <div class="clear"></div>
+            <h4 class="titrepartner">Partners</h4>
+           <div class="partners">
+                <div class="row" >
+                    
+                    <div class="span-6">
+                        <img src=<?php echo Yii::app()->request->baseUrl . '/images/logo.jpg'; ?>  />
+                        
+                    </div>
+                    
+                    <div class="span-6">
+                        <img src=<?php echo Yii::app()->request->baseUrl . '/images/logo.jpg'; ?>  />
+                        
+                    </div>
+                    
+                    <div class="span-6">
+                        <img src=<?php echo Yii::app()->request->baseUrl . '/images/logo.jpg'; ?>  />
+                        
+                    </div>
+                    
+                    <div class="span-6">
+                        <img src=<?php echo Yii::app()->request->baseUrl . '/images/logo.jpg'; ?>  />
+                        
+                    </div>
+                    
+                </div> 
+            
+            </div>
+            
+            <div class="clear"></div>
+            <div id="footer">
+                Copyright &copy; <?php echo date('Y'); ?> by The WebSide.<br/>
+                All Rights Reserved.<br/>
+            </div><!-- footer -->
+
+        </div><!-- page -->
+
+    </body>
+</html>
---
+++ protected/views/site/forwhoandhow0.php
@@ -0,0 +1,49 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle = Yii::app()->name;
+?>
+
+<h1 class="title">Why the Africa Press List ?</h1>
+<hr />
+<div class="row">
+    <div class="span-16" >   
+        <div class="homeparagraph">
+
+
+
+            The Africa Press List wishes to provide an effective and efficient solution to </br>
+            international organizations and companies that want to reach an (press) audience that </br>
+            is African or has an interest in Africa.</br>
+            </br>
+            </br>
+            If the following complies with your needs, we recommend you to sign-up.</br></br>
+
+            <ul >
+                <li class="li2">Social media integrated press release campaigns</li>
+                <li class="li2">You can personalize the search system in the database with own search terms</li>
+                <li class="li2">More than 30 thematic categories to select journalists and media titles</li>
+                <li class="li2">You can select entire Africa, regions, countries and cities</li>
+                <li class="li2">Draft your own selection lists and keep these online available</li>
+                <li class="li2">Keep a history of your press releases sent out</li>
+                <li class="li2">All press releases are published on the blog of Africa Business Communities and</li>
+                redistributed to the biggest Africa related Linkedin groups and twitter channels
+            </ul>
+
+            <p>In a later phase, we will launch the Africa Press List for local companies or organizations only active in some or only one country(ies) in Africa. Herewith, the Africa Press List provides flexible local solutions at low costs.</p>
+            &nbsp;</p>
+        
+        <hr />
+Stay up to date with our newest development and services.
+You can also follow us on social media.
+
+        </div>
+    </div>
+    <div class="span-12" >
+        
+        <a href="<?php echo Yii::app()->request->baseUrl ?>/user/create"><?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/addpressrel.jpg', ''); ?></a><br /><br />
+        <a href="<?php echo Yii::app()->request->baseUrl ?>/contact/create"> <?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/registerjour.jpg', ''); ?></a>
+    </div>
+</div>
+
+
