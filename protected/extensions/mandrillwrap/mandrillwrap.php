f8e3c53822b422f0cd40e8b9c979b79490572f29
aymenmas <aymenmas@localhost>
2014-08-01 01:19:22 +0100

    FriAug1 1:07

--- protected/extensions/mandrillwrap/mandrillwrap.php
+++ protected/extensions/mandrillwrap/mandrillwrap.php
@@ -12,41 +12,27 @@ class mandrillwrap extends CApplicationComponent {
 	
 	public function init()
 	{
-		Yii::import('application.vendors.mandrill.Mandrill');
-		define('MANDRILL_API_KEY','i4NfTgjttPYJhWA52Edd3w');
-	}
-	
-	public function sendEmail() {
+		Yii::import('application.vendors.Mandrill');
+		define('MANDRILL_API_KEY','-47REdJ11GUIcpHCv60c0w');
+        }
+		public function sendEmail() {		
 		
-		$request_json = '{
-						"type":"messages",
-						"call":"send",
-						"message": {
-							"subject":"' . $this->subject . '",
-							"to":[
-								{
-								"email": "' . $this->toEmail . '",
-								"name": "' . $this->toName . '"
-								}
-							],
-							"headers":
-								{
-									"...":"..."
-							},
-							"url_strip_qs":true,
-							"from_email": "' . $this->fromEmail . '",
-							"from_name": "' . $this->fromName . '",
-							"text": "' . $this->text . '",
-							"track_opens":false,
-							"track_clicks":false,
-							"auto_text":true,
-							"tags":["fal"],
-							"google_analytics_domains":["..."],
-							"google_analytics_campaign":["..."],
-							"metadata":["..."]
-							}
-						}';
+$request_json = '{
+"type":"messages",
+"call":"send",
+"message":{"html": "<h1>example html</h1>",
+ "text": "example text", 
+ "subject": "example subject", 
+ "from_email": "mohamedaymen.mastouri@esprit.tn", 
+ "from_name": "example from_name",
+ "to":[{"email": "mohamedaymen.mastouri@esprit.tn", "name": "Wes Widner"}],
+ "headers":{"...": "..."},
+"track_opens":false,
+"track_clicks":false
+}}';
+$ret = Mandrill::call((array) json_decode($request_json));
+print_r($ret);
+//$ret1 =Mandrill::call(array('type'=>'users', 'call'=>'ping'));
 
-		$ret = Mandrill::call((array) json_decode($request_json));
     }
 }
